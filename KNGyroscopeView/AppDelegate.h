0d304057ac6d0a8f43764eb11dcaecf37d3aa2b0
Leessang <992275294@qq.com>
2017-12-07 17:14:31 +0800

    Initial Commit

---
+++ KNGyroscopeView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KNGyroscopeView
+//
+//  Created by 刘凡 on 2017/12/7.
+//  Copyright © 2017年 KrystalName. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KNGyroscopeView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KNGyroscopeView
+//
+//  Created by 刘凡 on 2017/12/7.
+//  Copyright © 2017年 KrystalName. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

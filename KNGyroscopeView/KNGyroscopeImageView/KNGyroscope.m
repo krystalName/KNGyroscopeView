9d4f673c1f49ea48e76b532ccaed9381c46fa973
Leessang <992275294@qq.com>
2017-12-08 16:00:38 +0800

    陀螺仪初始化类写好

--- KNGyroscopeView/KNGyroscopeImageView/KNGyroscope.m
+++ KNGyroscopeView/KNGyroscopeImageView/KNGyroscope.m
@@ -14,9 +14,9 @@
     ///重力加速block
     void(^_startAccelerationOfGravityBlock)(float x,float y,float z);
     ///重力感应block
-    void(^_stratAccelerometerBlock)(float x,float y,float z);
+    void(^_startAccelerometerBlock)(float x,float y,float z);
     ///陀螺仪block
-    void(^_stratGyroscopeBlock)(float x,float y,float z);
+    void(^_startGyroscopeBlock)(float x,float y,float z);
 }
 
 #pragma mark - 单例实现
@@ -28,54 +28,6 @@
         gyroscope = [[self alloc]init];
     });
     return gyroscope;
-}
-
-
-
--(void)stratAccelerometerBlock:(void (^)(float, float, float))complete
-{
-    
-    //如果有重力感应
-    if (_manger.gyroAvailable) {
-        
-        //设置时间
-        _manger.gyroUpdateInterval = _timeInterval;
-        
-        //block
-        [_manger startGyroUpdatesToQueue:_queue withHandler:^(CMGyroData * _Nullable gyroData, NSError * _Nullable error) {
-            
-            if (error) {
-                //报错就停止重力感应
-                [_manger  stopGyroUpdates];
-                NSLog(@"%@",[NSString stringWithFormat:@"gryerror: %@",error]);
-            }
-            else
-            {
-                _stratAccelerometerBlock = complete;
-                
-                [self performSelectorOnMainThread:@selector(gyroUpdate:) withObject:gyroData waitUntilDone:NO];
-            }
-        }];
-    }else{
-        NSLog(@"设备没有重力感应");
-    }
-}
-
-
--(void)gyroUpdate:(CMGyroData *)gyroData;
-{
-    //分量
-    _stratAccelerometerBlock(gyroData.rotationRate.x,gyroData.rotationRate.y,gyroData.rotationRate.z);
-}
-
-
-
-
--(void)startAccelerationOfGravityBlock:(void (^)(float, float, float))complete
-{
-    if(_manger.accelerometerAvailable){
-        
-    }
 }
 
 
@@ -97,6 +49,118 @@
     _queue = [[NSOperationQueue alloc]init];
 }
 
+
+
+
+//开始重力感应
+-(void)stratAccelerometerBlock:(void (^)(float, float, float))complete
+{
+    
+    //如果有重力感应
+    if (_manger.gyroAvailable) {
+        
+        //设置时间
+        _manger.gyroUpdateInterval = _timeInterval;
+        
+        //block
+        [_manger startGyroUpdatesToQueue:_queue withHandler:^(CMGyroData * _Nullable gyroData, NSError * _Nullable error) {
+            
+            if (error) {
+                //报错就停止重力感应
+                [_manger  stopGyroUpdates];
+                NSLog(@"%@",error.localizedDescription);
+            }
+            else
+            {
+                _startAccelerometerBlock = complete;
+                
+                [self performSelectorOnMainThread:@selector(gyroUpdate:) withObject:gyroData waitUntilDone:NO];
+            }
+        }];
+    }else{
+        NSLog(@"设备没有重力感应");
+    }
+}
+
+
+-(void)gyroUpdate:(CMGyroData *)gyroData
+{
+    //分量
+    _startAccelerometerBlock(gyroData.rotationRate.x,gyroData.rotationRate.y,gyroData.rotationRate.z);
+}
+
+//开始重力加速
+-(void)startAccelerationOfGravityBlock:(void (^)(float, float, float))complete
+{
+    //如果是重力加速
+    if(_manger.accelerometerAvailable){
+        //设置时间
+        _manger.accelerometerUpdateInterval = _timeInterval;
+        
+        //开始加速
+        [_manger startAccelerometerUpdatesToQueue:_queue withHandler:^(CMAccelerometerData * _Nullable accelerometerData, NSError * _Nullable error) {
+           //如果报错
+            if (error) {
+                [_manger stopAccelerometerUpdates];
+                
+                NSLog(@"%@",error.localizedDescription);
+            }else{
+            
+                _startAccelerationOfGravityBlock = complete;
+                //回到主线程中
+                [self performSelectorOnMainThread:@selector(accelerometUpdate:) withObject:accelerometerData waitUntilDone:NO];
+            }
+        }];
+    }else{
+        NSLog(@"设备没有加速器");
+    }
+}
+
+-(void)accelerometUpdate:(CMAccelerometerData *)accelerometerData{
+    
+    _startAccelerationOfGravityBlock(accelerometerData.acceleration.x,accelerometerData.acceleration.y,accelerometerData.acceleration.z);
+}
+
+
+//开始陀螺仪
+-(void)stratGyroscopeBlock:(void (^)(float, float, float))complete{
+    
+    //判断有无重力陀螺仪
+    if (_manger.deviceMotionAvailable) {
+        _manger.deviceMotionUpdateInterval = _timeInterval;
+        
+        [_manger startDeviceMotionUpdatesToQueue:_queue withHandler:^(CMDeviceMotion * _Nullable motion, NSError * _Nullable error) {
+           
+            if (error) {
+                //停止陀螺仪
+                [_manger stopDeviceMotionUpdates];
+                NSLog(@"%@",error.localizedDescription);
+            }else{
+                _startGyroscopeBlock = complete;
+                //回到主线程
+                [self performSelectorOnMainThread:@selector(gyroscopeUpdate:) withObject:motion waitUntilDone:NO];
+            }
+        }];
+        
+    }else
+    {
+        NSLog(@"设备没有陀螺仪");
+    }
+}
+
+-(void)gyroscopeUpdate:(CMDeviceMotion *)motion
+{
+    _startGyroscopeBlock(motion.rotationRate.x,motion.rotationRate.y,motion.rotationRate.z);
+}
+
+
+//停止
+-(void)stop
+{
+    [_manger stopDeviceMotionUpdates];
+    [_manger stopGyroUpdates];
+    [_manger stopAccelerometerUpdates];
+}
 
 
 

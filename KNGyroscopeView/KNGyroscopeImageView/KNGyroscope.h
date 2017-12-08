e0ce3c9cfe9a9c69a2aaa8419eac08ebebcb9736
Leessang <992275294@qq.com>
2017-12-08 11:19:52 +0800

    创建陀螺仪的类

---
+++ KNGyroscopeView/KNGyroscopeImageView/KNGyroscope.h
@@ -0,0 +1,14 @@
+//
+//  KNGyroscope.h
+//  KNGyroscopeView
+//
+//  Created by 刘凡 on 2017/12/8.
+//  Copyright © 2017年 KrystalName. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreMotion/CoreMotion.h>
+
+@interface KNGyroscope : NSObject
+
+@end
---
+++ KNGyroscopeView/KNGyroscopeImageView/KNGyroscope.m
@@ -0,0 +1,13 @@
+//
+//  KNGyroscope.m
+//  KNGyroscopeView
+//
+//  Created by 刘凡 on 2017/12/8.
+//  Copyright © 2017年 KrystalName. All rights reserved.
+//
+
+#import "KNGyroscope.h"
+
+@implementation KNGyroscope
+
+@end
